0805f67955742dc14df58d3eed8d711e0aa33b3e
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-11-04 17:15:13 +0300

    Added demonstration of a part of the library's capabilities

--- main.c
+++ main.c
@@ -1,64 +1,162 @@
 #include <stdio.h>
+#include <malloc.h>
+
+/**
+ * Demonstration of a part of library features. Full functionality is described in the documentation.
+ */
 
 #include "collections/list/list.h"
 #include "collections/stack/stack.h"
+#include "collections/queue/queue.h"
+
+#include "collections/hashmap/hashmap.h"
+
+void intOutputArr(NodeInfo* arr, int size) {
+    printf("\n");
+    for(int i = 0; i < size; i++) {
+        printf("[%d]: %d\n", i, arr[i]);
+    }
+    printf("\n");
+}
+
+void listDemo() {
+    printf("---------- LIST DEMO ----------");
+    NodeInfo value;
+
+    // LISTS DEMO:
+    Node* list = list_create();
+    value.intValue = 1;
+    list_insertFirst(list, value);
+    printf("GET FIRST: %d\n", list_getFirst(list).intValue);
+
+    value.stringValue = "Test";
+    list_insertFirst(list, value);
+    value.intValue = 2;
+    list_insertLast(list,value);
+    printf("POP FIRST: %s\n", list_popFirst(list).stringValue);
+
+    value.intValue = 3;
+    list_insertLast(list, value);
+
+    value.intValue = 4;
+    list_insert(list, 1, value);
+
+    int size = list_size(list);
+    NodeInfo* arr = list_toArray(list);
+    intOutputArr(arr, size);
+    free(arr);
+
+    printf("POP LAST: %d", list_popLast(list).intValue);
+    arr = list_toArray(list);
+    intOutputArr(arr, list_size(list));
+    free(arr);
+
+    printf("POP FIRST: %d", list_popFirst(list).intValue);
+    intOutputArr(list_toArray(list), list_size(list));
+
+
+    printf("REMOVE 1th");
+    list_remove(list, 1);
+    arr = list_toArray(list);
+    intOutputArr(arr, list_size(list));
+    free(arr);
+
+    list_removeList(&list);
+    if(!list) {
+        printf("List removed\n");
+    }
+}
+
+void stackDemo() {
+    printf("\n\n\n---------- STACK DEMO ----------\n");
+    Node *stack = stack_create();
+
+    const n = 10;
+
+    printf("\nIS_EMPTY: %d\n", stack_isEmpty(stack));
+
+    printf("Placed values:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d\n", i);
+        Value value;
+        value.intValue = i;
+        stack_push(stack, value);
+    }
+    printf("\nIS_EMPTY: %d\n", stack_isEmpty(stack));
+
+    printf("\nSTACK: \n");
+    for(int i = 0; i< n; i++) {
+        printf("%d\n", stack_pop(stack).intValue);
+    }
+
+    stack_remove(&stack);
+    if(!stack) {
+        printf("Stack removed\n");
+    }
+}
+
+void queueDemo() {
+    printf("\n\n\n---------- QUEUE DEMO ----------\n");
+    Node *queue = queue_create();
+
+    const n = 10;
+
+    printf("\nIS_EMPTY: %d\n", queue_isEmpty(queue));
+
+    printf("Placed values:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", i);
+        Value value;
+        value.intValue = i;
+        queue_add(queue, value);
+    }
+    printf("\nIS_EMPTY: %d\n", queue_isEmpty(queue));
+
+    printf("\nQUEUE: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", queue_poll(queue).intValue);
+    }
+
+    stack_remove(&queue);
+    if(!queue) {
+        printf("Queue removed\n");
+    }
+}
+
+void hashMapDemo() {
+    printf("\n\n\n---------- HASHMAP DEMO ----------\n");
+    HashMap map = hashmap_create(100);
+    Value defaultValue;
+    defaultValue.intValue = -1;
+
+    Value value;
+
+    printf("\nIS_EMPTY: %d\n", hashmap_isEmpty(map));
+
+    value.intValue = 1;
+    hashmap_put(map, "Test", value);
+    printf("\nValue of key Test: %d\n", hashmap_get(map, "Test", defaultValue).intValue);
+
+    value.intValue = 6;
+    hashmap_put(map, "Test", value);
+    printf("\nValue of key Test: %d\n", hashmap_get(map, "Test", defaultValue).intValue);
+
+    if(hashmap_containsKey(map, "Test2")) {
+        hashmap_remove(map, "Test2");
+    } else {
+        printf("\nKey Test2 is not found\n");
+    }
+
+    hashmap_removeMap(&map);
+    if(!map.table) {
+        printf("\nHashMap removed\n");
+    }
+}
 
 int main() {
-
-    Node *head = list_create();
-    NodeInfo value;
-    printf("%d\n", list_size(head));
-    value.intValue = 2;
-    list_insertLast(head, value);
-    value.intValue = 3;
-    printf("%d\n", list_size(head));
-    list_insertLast(head, value);
-    printf("%d\n", list_size(head));
-    value.intValue = 4;
-    list_insertLast(head, value);
-
-    NodeInfo info;
-    info = list_get(head,1);
-    printf("%d", info.intValue);
-
-    info = list_getLast(head);
-    printf("%d", info.intValue);
-
-    info = list_getFirst(head);
-    printf("%d", info.intValue);
-
-    info = list_popFirst(head);
-    printf("%d", info.intValue);
-
-    info.intValue = -1;
-    list_insertFirst(head, info);
-
-    info.intValue = 10;
-    list_insert(head, 1, info);
-
-    NodeInfo *nodeInfo = list_toArray(head);
-    printf("\n\n");
-    for(int i = 0; i < list_size(head); i++) {
-        printf("%d | %d\n", nodeInfo[i].intValue, list_get(head, i));
-    }
-
-    list_remove(head, 1);
-    nodeInfo = list_toArray(head);
-    printf("\n\n");
-    for(int i = 0; i < list_size(head); i++) {
-        printf("%d | %d\n", nodeInfo[i].intValue, list_get(head, i));
-    }
-
-    Node *stack = stack_create();
-    for(int i = 0; i < 10; i++) {
-        NodeInfo info1;
-        info1.intValue = i;
-        stack_push(head, info1);
-    }
-
-    for(int i = 0; i < 10; i++) {
-        printf("%d", stack_pop(head).intValue);
-    }
-
+    listDemo();
+    stackDemo();
+    queueDemo();
+    hashMapDemo();
     return 0;
 }
